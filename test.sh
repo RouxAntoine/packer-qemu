0c6bf5032238869341c6b428c47401df99c86322
Antoine <antoinroux@hotmail.fr>
2020-02-14 07:56:41 +0100

    add ansible test inventory start provisionning vm disk

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-qemu-system-x86_64 -cdrom ./packer/assets/install-amd64-minimal-20200205T214502Z.iso -boot order=b -m 1024 -smp cpus=1,cores=4
+qemu-system-x86_64 \
+    -cdrom ./packer/assets/install-amd64-minimal-20200205T214502Z.iso \
+    -boot order=d -m 1024 \
+    -smp cpus=1,sockets=2,maxcpus=2 \
+    -drive "file=../../VirtualBox VMs/gentoo_snapshot.cow,format=qcow2,index=1" \
+    -device virtio-net,netdev=user.0 \
+    -netdev user,id=user.0,hostfwd=tcp::5556-:22
+
+
+# run playbook on aready running qemu vm
+# ansible-playbook -i inventory_test.ini playbook.yml --extra-vars "ssh_pub=$(cat ../id_rsa_qemu) geenrated_user=antoine generated_group=antoine"
+
