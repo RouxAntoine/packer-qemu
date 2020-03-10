933cd014fa1d14a65e6868136b1cc266e5f4e3bd
Antoine <antoinroux@hotmail.fr>
2020-03-11 00:43:33 +0100

    finish os stage 3 download extract, configure mount and chroot

--- packer/assets/ansible/run-ansible.sh
+++ packer/assets/ansible/run-ansible.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PLAYBOOK="${1:-playbook.yml}"
-DEBUG="-vvv"
+#DEBUG="-vvv"
+TAGS="--tags configure"
 
 # run playbook on aready running qemu vm
-ansible-playbook -i inventory_test.ini "$PLAYBOOK" --extra-vars "ssh_pub=$(cat ../id_rsa_qemu) generated_user=antoine generated_group=antoine"
+ansible-playbook $DEBUG -i inventory_test.ini $TAGS --extra-vars "ssh_pub=$(cat ../id_rsa_qemu) generated_user=antoine generated_group=antoine" $PLAYBOOK
